094a32487addd566e3ae5b1b5b341f72b49942b0
sathish <sathishped860@gmail.com>
2024-05-08 16:23:00 +0530

    adding network img by using Image.network() widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,30 +13,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          // crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
                 width: 100,
                 height: 100,
                 color: Color.fromARGB(255, 16, 21, 163)),
-            SizedBox(height: 20
-            ),
+            SizedBox(height: 20),
             Container(
-                width: 100,
-                height: 100,
-                color: Color.fromARGB(255, 16, 163, 58)),
-            SizedBox(height: 20
+              width: 100,
+              height: 100,
+              color: Color.fromARGB(255, 16, 163, 58),
+              child: Center(child: Text("Sathish")),
             ),
+            SizedBox(height: 20),
             Container(
                 width: 100,
                 height: 100,
                 color: Color.fromARGB(255, 163, 16, 104)),
-            SizedBox(height: 20
-            ),
+            SizedBox(height: 20),
             Container(
                 width: 100,
                 height: 100,
                 color: Color.fromARGB(255, 148, 163, 16)),
+                SizedBox(height: 20),
+            Image.network(
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+                "https://plus.unsplash.com/premium_photo-1664298849700-abbfe6f854d5?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8")
           ],
         ),
       ),
