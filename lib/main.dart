5d82b72f77d375b5c3a4451c37713ff4a410b1bb
sathish <sathishped860@gmail.com>
2024-05-08 13:44:58 +0530

    creating four coloms of container

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            body: Center(
-      child: Container(width: 100, height: 100, color: Colors.green),
-    )));
+      home: Scaffold(
+          body: Center(
+        child: Column(
+          children: [
+            Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(255, 16, 21, 163)),
+            Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(255, 16, 163, 116)),
+            Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(255, 163, 16, 104)),
+            Container(
+                width: 100,
+                height: 100,
+                color: Color.fromARGB(255, 148, 163, 16)),
+          ],
+        ),
+      )),
+    );
   }
 }
 
